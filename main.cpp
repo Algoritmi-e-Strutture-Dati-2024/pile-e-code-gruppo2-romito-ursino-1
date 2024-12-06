d9860a573419e06d6bf38700ba99544be7e93923
RomitoGiovanni <g.romito15@studenti.uniba.it>
2024-12-06 11:38:31 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Pila.cpp"  
+#include "Coda.cpp" 
 
 int main() 
 {
@@ -27,5 +28,34 @@ int main()
     // Visualizza l'elemento in cima alla pila dopo il pop
     pila.top();
 
-  //non dovrei stampare nulla, perche gia nella funzione richiamata fa tutto
+    
+    // Crea una coda di interi
+    Coda<int> coda;
+
+    // Aggiungi alcuni elementi alla coda
+    coda.enqueue(10);
+    coda.enqueue(20);
+    coda.enqueue(30);
+    coda.enqueue(40);
+
+    // Visualizza la dimensione della coda
+    coda.size();  // La dimensione dovrebbe essere 4
+
+    // Visualizza l'elemento in testa alla coda
+    coda.front();  // L'elemento in testa è 10
+
+    // Rimuovi un elemento dalla coda
+    coda.dequeue();  // Rimuove 10
+
+    // Visualizza la dimensione della coda dopo il dequeue
+    coda.size();  // La dimensione sarà 3
+
+    // Visualizza l'elemento in testa alla coda dopo il dequeue
+    coda.front();  // L'elemento in testa sarà 20
+
+    // Rimuovi tutti gli altri elementi
+    coda.dequeue();  // Rimuove 20
+    coda.dequeue();  // Rimuove 30
+
+  //non dovrei stampare nulla, perche gia nelle funzioni richiamate si fa tutto
 }
